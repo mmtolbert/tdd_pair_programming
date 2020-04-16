7707fee6d0599a39e71c2ece63108fdc04d95e26
mmtolbert <mmtolbert@cox.net>
2020-04-16 12:38:50 -0400

    Updated Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,16 +1,20 @@
 
 public class Rectange() {
 
-  public Rectangle(double height, double width){
+  private double height;
+  private double width;
 
+  public Rectangle(double height, double width){
+      this.height = height;
+      this.width = width;
   }
 
   public double getPerimeter() {
-
+    return ((this.height*2) + (this.width*2));
   }
 
   public double getArea() {
-    
+    return (this.height * this.width);
   }
 
 }
